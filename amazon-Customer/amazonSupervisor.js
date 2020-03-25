2ba9723a9273b159a6bba1d1b5c1f8071af59fc0
jrich2348 <55868611+jrich2348@users.noreply.github.com>
2020-03-25 18:13:14 -0400

    Update amazonSupervisor.js

--- amazon-Customer/amazonSupervisor.js
+++ amazon-Customer/amazonSupervisor.js
@@ -1,100 +1,95 @@
- var mysql = require("mysql");
+var mysql = require("mysql");
 var inquirer = require("inquirer");
-require("console.table");
 
- var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "amazon"
+var connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "root",
+    database: "bamazon_DB"
 });
 
- connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connection successful!");
-  makeTable();
-});
-
-function makeTable() {
-   connection.query("SELECT * FROM products", function(err, res) {
+//connect to the DB and call the superVisor function
+connection.connect(function (err) {
     if (err) throw err;
-    console.table(res);
-    promptSupervisor();
-  });
+    superVisor();
+});
+
+//function displays the prompt menu for the supervisor options
+function superVisor() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View Product Sales by Department",
+                "Create New Department",
+                "Exit"
+            ]
+        }).then(function (answer) {
+            switch (answer.action) {
+                case "View Product Sales by Department":
+                    departmentSales();
+                    break;
+
+                case "Create New Department":
+                    newDepartment();
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        });
 }
 
-function promptSupervisor() {
-   inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "choice",
-        message: "What would you like to do?",
-        choices: ["View Product Sales by Department", "Create New Department", "Quit"]
-      }
-    ])
-    .then(function(val) {
-       if (val.choice === "View Product Sales by Department") {
-        viewSales();
-      }
-      else if (val.choice === "Create New Department") {
-        addDepartment();
-      }
-      else {
-        console.log("Goodbye!");
-        process.exit(0);
-      }
-    });
-}
+//perform a join on the department and product tables to gather the necessary columns
+//departments are then grouped together (product sales amongst items in the same department are summed together)
+function departmentSales() {
+    var query = "SELECT d.department_name, d.over_head_cost, d.department_id,"
+    + " SUM(p.product_sales) AS product_sales ";
+	query += "FROM departments as d INNER JOIN products as p ";
+	query += "ON d.department_name = p.department_name ";
+	query += "GROUP BY department_id ";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
 
-function addDepartment() {
-   inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the name of the department?"
-      },
-      {
-        type: "input",
-        name: "overhead",
-        message: "What is the overhead cost of the department?",
-        validate: function(val) {
-          return val > 0;
+        for (var i = 0; i < res.length; i++) {
+            var profit = parseInt(res[i].product_sales) - parseInt(res[i].over_head_cost);
+            console.log("Department #: " + res[i].department_id + " || Department: " + res[i].department_name + " || Overhead Cost: $" + res[i].over_head_cost + " || Product Sales: $" + res[i].product_sales + " || Total Profit: $" + profit);
+            console.log("---")
         }
-      }
-    ])
-    .then(function(val) {
-       connection.query(
-        "INSERT INTO departments (department_name, over_head_costs) VALUES (?, ?)",
-        [val.name, val.overhead],
+        //call the supervisor menu function
+        superVisor();
+    });
+
+}
+
+//adds a new department
+function newDepartment() {
+    inquirer.prompt([{
+        name: "department",
+        type: "input",
+        message: "Name of the new Department"
+    },
+    {
+        name: "over_head_cost",
+        type: "input",
+        message: "What is the over head cost for the Department"
+    }]).then(function (answer) {
+        connection.query("INSERT INTO departments SET ?",
+        {
+          department_name: answer.department,
+          over_head_cost: answer.over_head_cost
+        },
         function(err) {
-          if (err) throw err;
-           console.log("ADDED DEPARTMENT!");
-          makeTable();
-        }
-      );
+            if (err) throw err;
+            console.log("New Department successfully added");
+            //call the supervisor menu function
+            superVisor();
+        });   
     });
-}
-
-function viewSales() {
-   connection.query(
-    " SELECT " +
-    "   d.department_id, " +
-    "   d.department_name, " +
-    "   d.over_head_costs, " +
-    "   SUM(IFNULL(p.product_sales, 0)) as product_sales, " +
-    "   SUM(IFNULL(p.product_sales, 0)) - d.over_head_costs as total_profit " +
-    "FROM products p " +
-    "   RIGHT JOIN departments d ON p.department_name = d.department_name " +
-    "GROUP BY " +
-    "   d.department_id, " +
-    "   d.department_name, " +
-    "   d.over_head_costs",
-    function(err, res) {
-      console.table(res);
-      promptSupervisor();
-    }
-  );
 }
